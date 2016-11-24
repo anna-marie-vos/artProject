8b21be707fc9cab4fb2eaf53e1dd60690c5dd628
anna-marie-vos <amvos@outlook.com>
2016-11-25 09:42:12 +1300

    overall strategy stated in js file

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
 });
+
+//get a random background when loading the web page
+
+//make a layer over it that's one color
+
+//when the mouse moves over it erase it, it erases the forground color
+
+//when 90% of the picture is revealed it reloads the webpage
