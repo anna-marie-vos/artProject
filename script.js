32e91a8f106b4793ca575c0649cf900309f1b99b
anna-marie-vos <amvos@outlook.com>
2016-11-25 10:59:24 +1300

    step 1: get background color to change using jQuery

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-
+newWallPaper();
 });
 
 //get a random background when loading the web page
+//find a random background generator API.
+ // https://api.desktoppr.co/1/wallpapers/random
+function newWallPaper(){
+  $('body').css("background-color", randomColor);
 
+}
+//for now just create a random background color.
+function randomColor(){
+  return 'red';
+}
 //make a layer over it that's one color
 
 //when the mouse moves over it erase it, it erases the forground color
