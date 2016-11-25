9a062e956f4845827e0e518b716c9f53a9257aed
anna-marie-vos <amvos@outlook.com>
2016-11-25 22:31:16 +1300

    API added, but not getting key to work

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 $(document).ready(function(){
 newWallPaper();
-createGrid();
-removeColor();
+//createGrid();
+//removeColor();
 });
-
+var URL = "";
 //get a random background when loading the web page
 //find a random background generator API.
  // https://api.desktoppr.co/1/wallpapers/random
 function newWallPaper(){
   // $('body').css("background-color", randomColor);
-  // $.ajax({
-  //   url:'https://api.desktoppr.co/1/wallpapers/random',
-  // });
-  $('body').css("background-image",'url('+ randomImage()+')');
+randomImage();
+console.log(URL);
+  $('body').css("background-image",'url('+ URL+')');
 
 }
 function randomImage(){
-  var arr = [
-    'https://awesomewallpapers.files.wordpress.com/2015/07/suwqdpp-imgur.jpg?w=597&h=398&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/07/teawdxf-imgur.jpg?w=456&h=285&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/05/xophyxb-imgur.jpg?w=456&h=257&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/05/vvnaluz-imgur.jpg?w=238&h=134&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/03/theoasis-2880x1800.jpg?w=432&h=270&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/02/tunnel_-1920x1200.jpg?w=432&h=270&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2015/02/waves_mac_fullhd.jpg?w=480&h=270&crop=1',
-    'https://awesomewallpapers.files.wordpress.com/2014/12/zimg-11.jpg?w=586&h=391&crop=1'
-  ];
-  return arr[randomNumber(arr.length)];
+  $.ajax({
+    url:'https://api.desktoppr.co/1/wallpapers/random',
+    dataType: 'json',
+    key: 'rRa-_N2W1W8Xz18T2dKd',
+    type: 'GET',
+    success: function(data){
+      console.log(data);
+      URL = data.response.image.preview.url;
+      //console.log(URL);
+    }
+  });
 }
+
+
+// function randomImage(){
+//   var arr = [
+//     'https://awesomewallpapers.files.wordpress.com/2015/07/suwqdpp-imgur.jpg?w=597&h=398&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/07/teawdxf-imgur.jpg?w=456&h=285&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/05/xophyxb-imgur.jpg?w=456&h=257&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/05/vvnaluz-imgur.jpg?w=238&h=134&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/03/theoasis-2880x1800.jpg?w=432&h=270&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/02/tunnel_-1920x1200.jpg?w=432&h=270&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2015/02/waves_mac_fullhd.jpg?w=480&h=270&crop=1',
+//     'https://awesomewallpapers.files.wordpress.com/2014/12/zimg-11.jpg?w=586&h=391&crop=1'
+//   ];
+//   return arr[randomNumber(arr.length)];
+// }
 //for now just create a random background color.
 // function randomColor(){
 //   var color = [
