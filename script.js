97a6983e03c952d8fca1a50015d346c90fe9decd
anna-marie-vos <amvos@outlook.com>
2016-11-26 16:19:16 +1300

    css linear gradient added to colors

--- script.js
+++ script.js
@@ -25,20 +25,6 @@ function randomImage(){
   });
 }
 
-
-// function randomImage(){
-//   var arr = [
-//     'https://awesomewallpapers.files.wordpress.com/2015/07/suwqdpp-imgur.jpg?w=597&h=398&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/07/teawdxf-imgur.jpg?w=456&h=285&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/05/xophyxb-imgur.jpg?w=456&h=257&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/05/vvnaluz-imgur.jpg?w=238&h=134&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/03/theoasis-2880x1800.jpg?w=432&h=270&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/02/tunnel_-1920x1200.jpg?w=432&h=270&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2015/02/waves_mac_fullhd.jpg?w=480&h=270&crop=1',
-//     'https://awesomewallpapers.files.wordpress.com/2014/12/zimg-11.jpg?w=586&h=391&crop=1'
-//   ];
-//   return arr[randomNumber(arr.length)];
-// }
 //add random colors to the front blocks
 function randomColor(){
   color = [
